55f134ef2b22b8964cf776be6f27d8df5c1f885b
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-05-24 19:04:13 +0700

    QHoan commit to add README file

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -1,7 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 import View from './view.js';
-
+import fracty from 'fracty';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _data;
@@ -105,9 +104,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ingredient.quantity
-            ? new Fraction(ingredient.quantity).toString()
-            : ''
+          ingredient.quantity ? new fracty(ingredient.quantity).toString() : ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ingredient.unit}</span>${
